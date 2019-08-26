80002575532f8f2166823cbca41a643814dc1423
Liubov Naiman <citymnk@gmail.com>
2019-08-26 21:54:24 +0700

    aldent - добавила

---
+++ aldent/js/slider.js
@@ -0,0 +1,76 @@
+(function($){
+
+  var screenWidth = $(window).width();
+
+  $('#reviews__slider .slider1__slides').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    appendArrows: $('#reviews__slider .slider1__nav'),
+    responsive: [
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+  $('#blog__slider .slider1__slides').slick({
+    responsive: [
+      {
+        breakpoint: 4000,
+        settings: "unslick",
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          swipeToSlide: true,
+          appendArrows: ('#blog__slider .slider1__nav'),
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          swipeToSlide: true,
+          appendArrows: ('#blog__slider .slider1__nav'),
+        }
+      }
+    ]
+  });
+
+  function equalHeight(sliderID) {
+    $(sliderID).find('.slick-slide').height('auto');
+    var slickTrack = $(sliderID).find('.slick-track');
+    var slickTrackHeight = $(slickTrack).outerHeight();
+    $(sliderID).find('.slick-slide').css('height', slickTrackHeight + 'px');
+  }
+
+  $('#reviews__slider .slider1__slides').on('setPosition', function(){
+    equalHeight('#reviews__slider');
+  });
+
+  $('#blog__slider .slider1__slides').on('setPosition', function(){
+    equalHeight('#blog__slider');
+  });
+
+  $(window).resize(function() {
+
+    var currScreeWidth = $(window).width();
+
+    if (currScreeWidth != screenWidth) {
+      equalHeight('#reviews__slider');
+      equalHeight('#blog__slider');
+    }
+
+    screenWidth = $(window).width();
+  });
+
+})(jQuery);

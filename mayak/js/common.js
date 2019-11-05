53345e2285377bde07787d46dcfb497faef48f0e
Liubov Naiman <citymnk@gmail.com>
2019-11-05 19:12:32 +0700

    ..

--- mayak/js/common.js
+++ mayak/js/common.js
@@ -208,28 +208,6 @@
   toggleHidden('.job',true);
 
 
-  // Анимация чисел в "Цель и миссия"
-  if ($('.mission__nums').length > 0) {
-    var show = true;
-    var countbox = ".mission__nums";
-    $(window).on("scroll load resize", function () {
-        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-        var w_height = $(window).height(); // Высота окна браузера
-        var d_height = $(document).height(); // Высота всего документа
-        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-            $('.mission__nums-circle span').css('opacity', '1');
-            $('.mission__nums-circle span').spincrement({
-                thousandSeparator: " ",
-                duration: 2400
-            });
-            show = false;
-        }
-    });
-  }
-
 
   /* Resize */
   $(window).resize(function(){

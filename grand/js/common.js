bb01fe2681a1196f0b664be1ae9edb874c5e6261
Liubov Naiman <citymnk@gmail.com>
2020-04-21 22:19:10 +0700

    a new site - grand

---
+++ grand/js/common.js
@@ -0,0 +1,209 @@
+(function($){
+
+  /* Для проверки resize, т.к. на мобильном Хроме при скролле срабывает resize (без изменения ширины) */
+  var screenWidth = $(window).width();
+
+  /* Инициализация wow */
+  wow = new WOW({mobile: false})
+  wow.init();
+
+  /* Нужно для IE и некоторых других браузеров, чтобы понимал svg спрайты во внешних файлах */
+  svg4everybody();
+
+  /* полифил для object-fit */
+  objectFitImages($('[data-object-fit-cover],[data-object-fit-contain]'), {watchMQ: true});
+
+  /* инициализация fancybox */
+  $(".fancybox").fancybox({
+    padding: 0,
+    scrolling: 'auto'
+  });
+
+  /* Плавный скролл к якорю для всех ссылок с классом "inner-link" */
+  $(function(){
+    $('.inner-link[href^="#"]').click(function(){
+      var _href = $(this).attr('href');
+      $('html, body').animate({scrollTop: $(_href).offset().top - 20 +'px'});
+      return false;
+    });
+  });
+
+  /* Плавный скролл "вверх" */
+  $('a[href^="#page_wr"]').click(function(event){
+    event.preventDefault;
+    var _href = $(this).attr('href');
+    $('html, body').animate({scrollTop: '0px'});
+    return false;
+  });
+
+  $('#header-burger').click(function() {
+    $(this).toggleClass('active');
+    $('.header .menu').toggleClass('active');
+  });
+
+  /* Открытие / закрытие модалок (кроме карты) */
+  $('a.open-modal').click(function(event){
+    event.preventDefault();
+    var _href = $(this).attr('href');
+    if ( $(_href).length > 0 ) {
+      $(_href).addClass('active');
+      $(_href + ' .modal__content').addClass('visible');
+    }
+  });
+
+  function closeModal() {
+    var activeCount = $('.modal.active').length;
+    if ( activeCount > 1 ) {
+      $($('.modal.active')[activeCount - 1]).removeClass('active');
+      $('.modal__callback2 .modal__content').removeClass('visible');
+    } else {
+      $('.modal').removeClass('active');
+      $('.modal__content').removeClass('visible');
+    }
+  }
+
+  $(document).mouseup(function (e) {
+    var container = $(".modal .modal__content");
+    if (container.has(e.target).length === 0){
+      closeModal();
+    }
+  });
+
+  $('.modal .close').click(function(){
+    closeModal();
+  });
+
+  $(document).keydown(function(eventObject){
+    if( eventObject.which == 27 ){
+      closeModal();
+    }
+  });
+
+  // маска поля tel
+  $(".wpcf7-tel").mask("+7 (999) 999-99-99");
+
+  // Секции по типу "Вопрос - ответ"
+  function toggleHidden(xParent,hideOther) {
+    $(xParent + ' .top').click(function() {
+      $(this).find('.open').toggleClass('active');
+      $(this).siblings().slideToggle();
+      if (hideOther) {
+        $(this).parent().siblings().find('.open').removeClass('active');
+        $(this).parent().siblings().find('.hidden').slideUp();
+      }
+    });
+  }
+
+  toggleHidden('.faq',true);
+
+  /* Анимация чисел */
+  if ($('.num__list').length > 0) {
+    var show = true;
+    var countbox = ".scr_num";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.num__list .num__num span').css('opacity', '1');
+            $('.num__list .num__num span').spincrement({
+                thousandSeparator: " ",
+                duration: 3000
+            });
+            show = false;
+        }
+    });
+  }
+
+
+  // фиксированные шапка
+  $(window).on("scroll", function() {
+    var fromTop = $(document).scrollTop();
+    $(".header").toggleClass("fixed", (fromTop > 10));
+    $(".to-top").toggleClass("fixed", (fromTop > 600));
+  });
+
+
+  // Табы
+  // $('.prod-item__nav a').click(function(event) {
+  //   event.preventDefault();
+  //   var _href = $(this).attr('href');
+  //   $(this).parent().siblings().removeClass('current');
+  //   $(this).parent().addClass('current');
+  //   $('.services__tab').not(_href).hide();
+  //   $(_href).fadeIn();
+  //   if (window.matchMedia('(max-width: 767px)').matches) {
+  //     $('html, body').animate({scrollTop: $(_href).offset().top - 70 +'px'});
+  //   }
+  // });
+
+  $('img').on('dragstart', function (event) {
+    event.preventDefault();
+  });
+
+
+  /* СЛАЙДЕРЫ */
+  // function equalHeightSwiper(sliderID) {
+  //   $(sliderID).find('.swiper-slide').height('auto');
+  //   var slickTrackHeight = Math.max($(sliderID).find('.swiper-slide').outerHeight());
+  //   $(sliderID).find('.swiper-slide').css('height', slickTrackHeight + 'px');
+  // }
+
+  var quantitySlider = new Swiper('#quantity__slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    watchSlidesProgress: true,
+    autoplay: {
+      delay: 8000,
+    },
+    pagination: {
+      el: '.quantity__slider-btns .swiper-dots',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+  });
+
+  var projSlider = new Swiper('#proj__slider', {
+    slidesPerView: 2,
+    spaceBetween: 35,
+    watchSlidesProgress: true,
+    watchOverflow: true,
+    loop: true,
+    autoplay: {
+      delay: 8000,
+    },
+    navigation: {
+      nextEl: '.proj__slider-nav .swiper-button-next',
+      prevEl: '.proj__slider-nav .swiper-button-prev',
+    },
+    pagination: {
+      el: '.proj__slider-nav .swiper-dots',
+    },
+    breakpoints: {
+      575: {
+        slidesPerView: 3,
+      },
+      400: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  $(window).resize(function() {
+
+    var currScreeWidth = $(window).width();
+
+    if (currScreeWidth != screenWidth) {
+
+    }
+
+    screenWidth = $(window).width();
+  });
+
+})(jQuery);

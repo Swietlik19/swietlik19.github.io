00c2809708b6a80db003239d548186cc89d68ad8
Liubov Naiman <citymnk@gmail.com>
2021-03-03 14:34:23 +0700

    added new pages

--- greenhall/js/common.js
+++ greenhall/js/common.js
@@ -137,23 +137,24 @@
   /* Анимация чисел */
   var show = true;
   var countbox = ".scr2__wrap";
-  $(window).on("scroll load resize", function () {
-    if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-    var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-    var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-    var w_height = $(window).height(); // Высота окна браузера
-    var d_height = $(document).height(); // Высота всего документа
-    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-    if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-        $('.scr2__num strong').css('opacity', '1');
-        $('.scr2__num strong').spincrement({
-            thousandSeparator: " ",
-            duration: 2000
-        });
-        show = false;
-    }
-  });
-
+  if ($(countbox).legnth >= 0) {
+    $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      var w_height = $(window).height(); // Высота окна браузера
+      var d_height = $(document).height(); // Высота всего документа
+      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+          $('.scr2__num strong').css('opacity', '1');
+          $('.scr2__num strong').spincrement({
+              thousandSeparator: " ",
+              duration: 2000
+          });
+          show = false;
+      }
+    });
+  }
 
   $('.plans__stages-wrap').click(function(e){
     e.preventDefault();

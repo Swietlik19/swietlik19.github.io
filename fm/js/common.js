33f026ef910615d939eae312967c1fdf345c5a79
Liubov Naiman <citymnk@gmail.com>
2021-04-16 14:49:24 +0700

    added new page

---
+++ fm/js/common.js
@@ -0,0 +1,151 @@
+(function($){
+
+  /* Для проверки resize, т.к. на мобильном Хроме при скролле срабывает resize (без изменения ширины) */
+  var screenWidth = $(window).width();
+
+  /* Инициализация wow */
+  wow = new WOW({mobile: false})
+  wow.init();
+
+  /* Нужно для IE и некоторых других браузеров, чтобы понимал svg спрайты во внешних файлах */
+  svg4everybody();
+
+  /* полифил для object-fit */
+  objectFitImages($('[data-object-fit-cover],[data-object-fit-contain]'), {watchMQ: true});
+
+  /* инициализация fancybox */
+  $(".fancybox").fancybox({
+    padding: 0,
+    scrolling: 'auto'
+  });
+
+  /* Плавный скролл к якорю для всех ссылок с классом "inner-link" */
+  $(function(){
+    $('.inner-link[href^="#"]').click(function(){
+      var _href = $(this).attr('href');
+      $('html, body').animate({scrollTop: $(_href).offset().top - 20 +'px'});
+      return false;
+    });
+  });
+
+  /* Плавный скролл "вверх" */
+  $('a[href^="#page_wr"]').click(function(event){
+    event.preventDefault;
+    var _href = $(this).attr('href');
+    $('html, body').animate({scrollTop: '0px'});
+    return false;
+  });
+
+  $('.burger').click(function() {
+    $('.burger').toggleClass('active');
+    $('.side-menu').toggleClass('active');
+  });
+
+  /* Открытие / закрытие модалок (кроме карты) */
+  $('a.open-modal').click(function(event){
+    event.preventDefault();
+    var _href = $(this).attr('href');
+    if ( $(_href).length > 0 ) {
+      $(_href).addClass('active');
+      $(_href + ' .modal__content').addClass('visible');
+    }
+  });
+
+  function closeModal() {
+    var activeCount = $('.modal.active').length;
+    if ( activeCount > 1 ) {
+      $($('.modal.active')[activeCount - 1]).removeClass('active');
+      $('.modal__callback2 .modal__content').removeClass('visible');
+    } else {
+      $('.modal').removeClass('active');
+      $('.modal__content').removeClass('visible');
+    }
+  }
+
+  $(document).mouseup(function (e) {
+    var container = $(".modal .modal__content");
+    if (container.has(e.target).length === 0){
+      closeModal();
+    }
+  });
+
+  $('.modal .close').click(function(){
+    closeModal();
+  });
+
+  $(document).keydown(function(eventObject){
+    if( eventObject.which == 27 ){
+      closeModal();
+    }
+  });
+
+  // маска поля tel
+  $(".wpcf7-tel").mask("+7 (999) 999-99-99");
+
+  // Секции по типу "Вопрос - ответ"
+  function toggleHidden(xParent,hideOther) {
+    $(xParent + ' .top').click(function() {
+      $(this).find('.open').toggleClass('active');
+      $(this).siblings().slideToggle();
+      if (hideOther) {
+        $(this).parent().siblings().find('.open').removeClass('active');
+        $(this).parent().siblings().find('.hidden').slideUp();
+      }
+    });
+  }
+
+  toggleHidden('.faq',true);
+
+  // Кнопка "вверх"
+  $(window).on("scroll", function() {
+    var fromTop = $(document).scrollTop();
+    $(".to-top").toggleClass("fixed", (fromTop > 600));
+  });
+
+  $('img').on('dragstart', function (event) {
+    event.preventDefault();
+  });
+
+
+  /* СЛАЙДЕРЫ */
+
+  var brandsSlider = new Swiper('#brands__slider', {
+    slidesPerView: 2,
+    spaceBetween: 8,
+    watchSlidesProgress: true,
+    loop: true,
+    navigation: {
+      nextEl: '.brands__slider-btns .swiper-button-next',
+      prevEl: '.brands__slider-btns .swiper-button-prev',
+    },
+    pagination: {
+      el: '.brands__slider-btns .swiper-dots',
+    },
+    breakpoints: {
+      767: {
+        slidesPerView: 4,
+        spaceBetween: 29,
+      },
+      650: {
+        slidesPerView: 4,
+        spaceBetween: 15,
+      },
+      500: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+    },
+  });
+
+  $(window).resize(function() {
+
+    var currScreeWidth = $(window).width();
+
+    if (currScreeWidth != screenWidth) {
+
+    }
+
+    screenWidth = $(window).width();
+  });
+
+})(jQuery);

787a590d95ba09860037eac29b4dcdaffc7553e8
Liubov Naiman <citymnk@gmail.com>
2021-04-28 15:42:50 +0700

    added modal, fixed fonts

--- KDK/js/toUp.js
+++ KDK/js/toUp.js
@@ -37,6 +37,44 @@ $(function(){
     $(this).parent().siblings().find('ul').slideUp();
   });
 
+  /* Открытие / закрытие модалок (кроме карты) */
+  $('a.open-modal').click(function(event){
+    event.preventDefault();
+    var _href = $(this).attr('href');
+    if ( $(_href).length > 0 ) {
+      $(_href).addClass('active');
+      $(_href + ' .modal__content').addClass('visible');
+    }
+  });
+
+  function closeModal() {
+    var activeCount = $('.modal.active').length;
+    if ( activeCount > 1 ) {
+      $($('.modal.active')[activeCount - 1]).removeClass('active');
+      $('.modal__project2 .modal__content').removeClass('visible');
+    } else {
+      $('.modal').removeClass('active');
+      $('.modal__content').removeClass('visible');
+    }
+  }
+
+  // $(document).mouseup(function (e) {
+  //   var container = $(".modal .modal__content");
+  //   if (container.has(e.target).length === 0 && !($(e.target).hasClass('modal__content')) ){
+  //     closeModal();
+  //   }
+  // });
+
+  $('.modal .close').click(function(){
+    closeModal();
+  });
+
+  $(document).keydown(function(eventObject){
+    if( eventObject.which == 27 ){
+      closeModal();
+    }
+  });
+
   
   $('.burger').click(function() {
     $('.burger').toggleClass('active');
@@ -106,6 +144,61 @@ $(function(){
           spaceBetween: 25,
         },
       },
+    });
+
+    var modalSlider = new Swiper('#modal__slider', {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      watchSlidesProgress: true,
+      watchOverflow: true,
+      pagination: {
+        el: '.modal__slider-btns .swiper-dots',
+      },
+      breakpoints: {
+        1070: {
+          slidesPerView: 3,
+          spaceBetween: 25,
+        },
+        400: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+      },
+    });
+
+    var modalThumbs = new Swiper('#modal__thumbs', {
+      slidesPerView: 2,
+      spaceBetween: 8,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      centerInsufficientSlides: true,
+      breakpoints: {
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 25,
+        },
+        870: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      },
+    });
+
+    var modalGallery = new Swiper('#modal__gallery', {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      watchSlidesProgress: true,
+      watchOverflow: true,
+      navigation: {
+        nextEl: '.modal__gallery-btns .swiper-button-next',
+        prevEl: '.modal__gallery-btns .swiper-button-prev',
+      },
+      pagination: {
+        el: '.modal__gallery-btns .swiper-dots',
+      },
+      thumbs: {
+        swiper: modalThumbs
+      }
     });
 
 
